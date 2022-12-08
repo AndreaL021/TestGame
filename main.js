eccb4046738bbb51771b76b9ef68542e67c382a1
AndreaL021 <88035660+AndreaL021@users.noreply.github.com>
2022-12-08 21:21:23 +0100

    prima versione

--- main.js
+++ main.js
@@ -1,217 +1,242 @@
-window.onload= function() {
-    var c= document.getElementById('canvas');
-    var context= c.getContext('2d');
+window.onload = function () {
+    var c = document.getElementById('canvas');
+    var context = c.getContext('2d');
+    let hard = document.getElementById('hard');
+    let hardLabel = document.getElementById('hardLabel');
+    hardLabel.style.display = "inline";
 
+    //////////   GIOCO   //////////
+    document.onkeydown= function (key) {
+        if (key.keyCode===32||key.keyCode===13) {
+            start.onclick();
+        }
+    }
+    let start = document.getElementById('start');
+    let up = document.getElementById('up');
+    let left = document.getElementById('left');
+    let down = document.getElementById('down');
+    let right = document.getElementById('right');
+    let blockup=false;
+    let blockleft=false;
+    let blockdown=false;
+    let blockright=false;
+    let score = 0;
+    let interval;
+    let i;
+    up.style.display = "none";
+    left.style.display = "none";
+    down.style.display = "none";
+    right.style.display = "none";
 
-    context.strokeStyle = "red";
-    context.lineWidth = 3;
+    let x = 100;
+    let y = 200;
+    let h=50;
+    let w=50;
+    let registro=[[0,0]];
+    let registro_reale=[];
 
-    context.beginPath();
-    context.rect(0, 0, 1500, 600);
-    context.fillStyle = "#1775b7";
-    context.fill();
-    // context.stroke(); 
-
-
-    context.beginPath();
-    context.moveTo(100, 100);
-    context.lineTo(50, 200);
-    context.lineTo(150, 200);
-    context.lineTo(100, 100);
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(550,200)
-    context.arc(400, 200, 150, 0, 2 * Math.PI);
-    context.fillStyle = "green";
-    context.fill();
-    context.stroke(); 
-
-    context.beginPath();
-    context.fillStyle = "red";
-    context.font = '50px Arial';
-    context.fillText("Hello World", 270, 200); 
-    context.lineWidth = 2;
-    context.strokeStyle = "blue";
-    context.strokeText("Hello World", 270, 200);
-
-    context.beginPath();
-    var img = new Image();
-    img.src = 'https://blob.sololearn.com/avatars/sololearn.jpg';
-    img.onload = function() {
-        context.drawImage(img, 700, 100, 200, 200);
-    };
-    
-//////////   GIOCO   //////////
-
-    let start=document.getElementById('start');
-    let jump=document.getElementById('jump');
-    let up=document.getElementById('up');
-    let left=document.getElementById('left');
-    let down=document.getElementById('down');
-    let right=document.getElementById('right');
-    let score=0;
-    jump.style.display="none";
-    up.style.display="none";
-    left.style.display="none";
-    down.style.display="none";
-    right.style.display="none";
-
-    let x = 300;
-    let y = 150;
     let t = Date.now();
     let fps = 0;
     let fpsSpan;
-    let speed = 500;
-    let dirx=0;
-    let diry=0;
-    var coinx = 15 + (Math.random() * (1500-50)); 
+    let speed = 250;
+    let dir = -1;
+    var coinx = 25+Math.floor(Math.random()*30)*50;
+    var coiny = 25+Math.floor(Math.random()*12)*50;
 
-    var coiny = 15 + (Math.random() * (600-50));
     function draw() {
-        // TEST
-            timePassed=(Date.now()-t)/1000;
-            t=Date.now();
-            fps = Math.round(1 / timePassed);
+        //Gestione animazione in base al frame rate
+        let timePassed = (Date.now() - t) / 1000;
+        t = Date.now();
+        fps = Math.round(1 / timePassed);
         //Pulisci campo di gioco
-            context.clearRect(0, 0, 1500, 600);
-
+        context.clearRect(0, 0, 1500, 600);
         //Ridisegna campo di gioco
-            context.beginPath();
-            context.rect(0, 0, 1500, 600);
-            // context.fillStyle = "#1775b7";
-            // context.fill();
-            context.lineWidth = 3;
-            context.stroke(); 
+        for (let y = 0; y < 600; y += 50) {
+            for (let x = 0; x < 1500; x += 50) {
+                context.beginPath();
+                context.rect(x, y, w, h);
+                context.lineWidth = 1;
+                context.stroke();
+            }
+        }
+        // SPOSTAMENTO/MORTE
+        switch (dir) {
+            case 0:
+                x < 1450 ? x += 50 : death();
+                break;
+            case 1:
+                x > 0 ? x -= 50 : death();
+                break;
+            case 2:
+                y < 550 ? y += 50 : death();
+                break;
+            case 3:
+                y > 0 ? y -= 50 : death();
+                break;
+        }
+        for (let i = 0; i < registro_reale.length; i++) {
+            if (registro_reale[i][0]==x && registro_reale[i][1]==y && i != registro_reale.length-1) {
+                death();
+            }
+        }
+        registro_reale.forEach((r)=>{
+        });
+        registro.push([x, y])
+        i=false;
 
-        //Ridisegna forma   
-            context.beginPath();
-            context.rect(x, y, 100, 50);
-            context.fillStyle="red";
-            context.fill();
+        // collisioni
+        if (coinx - 20 <= x + w && x <= coinx + 20 && coiny - 20 <= y + h && y <= coiny + 20) {
+            score += 1;
+            coinRespawn();
+        }
+        registro_reale=[];
         //COIN  
+        context.beginPath();
+        context.arc(coinx, coiny, 20, 0, 2 * Math.PI);
+        context.fillStyle = "gold";
+        context.fill();
+        context.strokeStyle = "black";
+        context.lineWidth = 1;
+        context.stroke();
+        //Ridisegna forma   
+        for (let i = 1; i <= score+1; i++) {
             context.beginPath();
-            context.arc(coinx, coiny, 15, 0, 2 * Math.PI);
-            context.fillStyle="gold";
+            context.rect(
+                registro[registro.length-i][0], //x
+                registro[registro.length-i][1], //y
+                w, 
+                h
+            );
+            context.fillStyle = "red";
             context.fill();
-            context.strokeStyle="black";
-            context.lineWidth = 1;
-            context.stroke();
+            registro_reale.push([
+                registro[registro.length-i][0], //x
+                registro[registro.length-i][1]  //y
+            ]);
+            
+        }
+        blockdown=false;
+        blockleft=false;
+        blockright=false;
+        blockup=false;
+
 
         //Disegna punteggio
-            context.beginPath();
-            context.font = '25px Arial';
-            context.fillStyle = 'black';
-            context.fillText("Score: " + score, 20, 30);
+        context.beginPath();
+        context.font = 'bold 25px Arial';
+        context.fillStyle = 'black';
+        context.fillText("Score: " + score, 20, 30);
+        context.fillText("FPS: " + fpsSpan, 1400, 30);
+    }
+    function coinRespawn() {
+        do {
+            i=false;
+            coinx = 25+Math.floor(Math.random()*30)*50;
+            coiny = 25+Math.floor(Math.random()*12)*50;
+            registro_reale.forEach((r)=>{
+                if (r[0]==coinx-25&& r[1]==coiny-25) {
+                    i=true;
+                }
+            });
+         
+        } while (i==true);
         
-        //Gestione animazione in base al frame rate
-        //dir=direzione
-            if((dirx == 1 && x<1400) || (dirx == -1 && x>0)) { 
-                x += (speed * timePassed * dirx);
-            } 
-            if((diry == 1 && y<550) || (diry == -1 && y>0)) { 
-                y += (speed * timePassed * diry);
-            } 
-            if (coinx-15<=x+100 && x<=coinx+15 && coiny-15<=y+50 && y<=coiny+15) {
-                score+=1;
-                coinx = 15 + (Math.random() * (1500-50)); 
-                coiny = 15 + (Math.random() * (600-50));
-                console.log(coinx);
-                console.log(coiny);
+    }
+    function death() {
+        document.onkeydown= function (key) {
+            if (key.keyCode===32||key.keyCode===13) {
+                start.onclick();
             }
-            context.fillText("FPS: " + fpsSpan, 1400, 30);
-        //Richiedi nuovo frame
-            window.requestAnimationFrame(draw);
+        };
+        hardLabel.style.display = "inline";
+        clearInterval(interval);
+        context.beginPath();
+        context.fillStyle = "red";
+        context.font = 'bold 50px Arial';
+        context.fillText("Game Over", 600, 230);
+        context.beginPath();
+        context.fillStyle = "red";
+        context.font = 'bold 40px Arial';
+        context.fillText("Press spacebar or enter to restart", 420, 300);
+        context.font = 'bold 40px Arial';
+        context.fillText("Score: "+score, 670, 360);
+        
     }
     function frames() {
-        fpsSpan=fps;
+        fpsSpan = fps;
     }
-    start.onclick = function() {
-        start.style.display="none";
-        jump.style.display="inline";
-        up.style.display="inline";
-        left.style.display="inline";
-        down.style.display="inline";
-        right.style.display="inline";
+    // if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    //     // Codice da eseguire nel caso di un dispositivo mobile
+    //     start.style.display = "inline";
+    // }
+    start.onclick = function () {
+        hardLabel.style.display = "none";
+        speed =  hard.checked===false ? 250 : 150;
+        registro=[[0,0]];
+        dir=-1;
+        x=100;
+        y=200;
+        dir=0;
+        score=0;
+        document.onkeydown= function (key) {
+            if (
+                    key.keyCode==87 && blockup==false
+                    ||key.keyCode===38 && blockup==false
+                ) {
+                dir!=2 ? dir=3 : dir=2;
+                blockleft=true;
+                blockright=true;
+            }else if (
+                    key.keyCode==65 && blockleft==false
+                    ||key.keyCode===37 && blockleft==false
+                ) {
+                dir!=0 ? dir=1 : dir=0;
+                blockdown=true;
+                blockup=true;
+            } else if(
+                    key.keyCode==83 && blockdown==false
+                    ||key.keyCode===40 && blockdown==false
+                ){
+                dir!=3 ? dir=2 : dir=3;
+                blockleft=true;
+                blockright=true;
+            }else if(
+                    key.keyCode==68 && blockright==false
+                    ||key.keyCode===39 && blockright==false
+                ){
+                dir!=1 ? dir=0 : dir=1;
+                blockdown=true;
+                blockup=true;
+            }
+            // w-a-s-d
+            // 87-65-83-68
+            // ↑|←|↓|→
+            // 38-37-40-39
+        }
+        start.style.display = "none";
+        up.style.display = "inline";
+        left.style.display = "inline";
+        down.style.display = "inline";
+        right.style.display = "inline";
         draw();
-        setInterval(frames, 100); 
+        interval= setInterval(draw, speed);
+        setInterval(frames, 100);
         context.clearRect(0, 0, 1500, 600);
     }
-    // jump.onkeyup = function() {
-    jump.onclick = function() {
-        y = (y-70)>= 0 ? y-70 : 0;
-    }
+
     // RIGHT
-    right.onmousedown = function() {dirx = 1;}
-    right.onmouseup = function() {dirx = 0;} 
-    right.ontouchstart = function() {dirx = 1;}
-    right.ontouchend = function() {dirx = 0;}
+    right.onmousedown = function () { dir!=1 ? dir=0 : dir=1 };
+    right.ontouchend = function () { dir!=1 ? dir=0 : dir=1 };
 
     // LEFT
-    left.onmousedown = function() {dirx = -1;}
-    left.onmouseup = function() {dirx = 0;} 
-    left.ontouchstart = function() {dirx = -1;}
-    left.ontouchend = function() {dirx = 0;} 
+    left.onmousedown = function () { dir!=0 ? dir=1 : dir=0 };
+    left.ontouchend = function () { dir!=0 ? dir=1 : dir=0 };
 
     // UP
-    up.onmousedown = function() {diry = -1;}
-    up.onmouseup = function() {diry = 0;} 
-    up.ontouchstart = function() {diry = -1;}
-    up.ontouchend = function() {diry = 0;}
+    up.onmousedown = function () { dir!=2 ? dir=3 : dir=2 };
+    up.ontouchend = function () { dir!=2 ? dir=3 : dir=2 };
 
     // DOWN
-    down.onmousedown = function() {diry = 1;}
-    down.onmouseup = function() {diry = 0;} 
-    down.ontouchstart = function() {diry = 1;}
-    down.ontouchend = function() {diry = 0;}
+    down.onmousedown = function () { dir!=3 ? dir=2 : dir=3 };
+    down.ontouchend = function () { dir!=3 ? dir=2 : dir=3 };
 }
-
-// OLD DRAW
-// function draw() {
-//     // TEST
-//         timePassed=(Date.now()-t)/1000;
-//         t=Date.now();
-//         fps = Math.round(1 / timePassed);
-//         // if(count==10)
-//         //     debugger;
-//     //Pulisci campo di gioco
-//         context.clearRect(0, 0, 1500, 600);
-
-//     //Ridisegna campo di gioco
-//         context.beginPath();
-//         context.rect(0, 0, 1500, 600);
-//         // context.fillStyle = "#1775b7";
-//         // context.fill();
-//         context.lineWidth = 3;
-//         context.stroke(); 
-
-//     //Ridisegna cerchio   
-//         context.beginPath();
-//         context.arc(x, y, 30, 0, 2 * Math.PI);
-//         context.fillStyle="red";
-//         context.fill();
-
-//     //Disegna punteggio
-//         context.beginPath();
-//         context.font = '25px Arial';
-//         context.fillStyle = 'black';
-//         context.fillText("Score: " + count, 20, 30);
-    
-//     //Gestione animazione in base al frame rate
-//     //dir=direzione
-//         x += dirx*(speed * timePassed);
-//         y += diry*(speed /4 * timePassed);
-//         // console.log(speed * timePassed);
-//         // debugger; 
-//     //Cambia posizione cerchio per il prossimo frame
-//         // x = x+7;
-//         if (x >= 1500-30 || x<=30) {
-//             dirx*=-1;
-//         }
-//         if (y >= 600-30 || y<=30) {
-//             diry*=-1;
-//         }
-//         context.fillText("FPS: " + fpsSpan, 1400, 30);
-//     //Richiedi nuovo frame
-//         window.requestAnimationFrame(draw);
-// }
